608d1cb0372fb89d15be01f783f91d3e1b20a3a9
William Koller <wkoller25@gmail.com>
2020-10-07 01:48:39 -0300

    feat: add MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-
+import { Module } from '@nestjs/common'
+import { BooksController } from './Controllers/books/books.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import Configuration from './Config/Configuration'
+import { BooksService } from './Services/books/books.service'
+import { BookRepository } from './Mongo/Repository/book.repository'
+import { BookSchema } from './Mongo/Schemas/book.schema'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forRoot(`${Configuration.mongoUri}`),
+    MongooseModule.forFeature([
+      { name: 'books', schema: BookSchema }
+    ])
+  ],
+  controllers: [BooksController],
+  providers: [BooksService, BookRepository],
 })
-export class AppModule {}
+export class AppModule { }
